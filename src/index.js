d80a5ffc8ab3bcaa79a4c4529a5b5146696bc46d
Manon Bouquin <Manon.bouquin@hotmail.fr>
2024-09-18 15:30:02 +0200

    Modification

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import "./styles/index.css";
-import App from "./components/App"
+import React from "react";
+import AppRoutes from "./AppRoutes";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import "./SASS/index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AppRoutes />
 );
 
 // If you want to start measuring performance in your app, pass a function
