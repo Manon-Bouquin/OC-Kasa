925285717fd5e3abb727c0bc7d51541b9f34909e
Manon Bouquin <Manon.bouquin@hotmail.fr>
2024-09-26 10:24:49 +0200

    Modification des fichiers

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Structure/Layout";
 import Accueil from "./pages/Accueil/Accueil";
 import Apropos from "./pages/Apropos/Apropos";
+import Logement from "./pages/Logement/Logement";
 import Error from "./components/Error/Erreur";
 
 function AppRoutes() {
@@ -11,6 +12,7 @@ function AppRoutes() {
                 <Route path="/" element={<Layout />}>
                     <Route path="/" element={<Accueil />}/>
                     <Route path="/apropos" element={<Apropos />}/>
+                    <Route path="/logement/:id" element={<Logement />}/>
                     <Route path="*" element={<Error />}/>
                 </Route>
             </Routes>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import reportWebVitals from "./reportWebVitals";
 import "./SASS/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <AppRoutes />
+    <AppRoutes />
 );
 
 // If you want to start measuring performance in your app, pass a function
