d05fa88ae9e271812b6d5b4e8ed364691369fbba
Manon Bouquin <Manon.bouquin@hotmail.fr>
2024-10-01 14:46:20 +0200

    Ajout mediamobile page Error

--- src/components/Error/Erreur.jsx
+++ src/components/Error/Erreur.jsx
@@ -4,7 +4,8 @@ function Error() {
     return (
         <div className="error-container">
             <h1 className="error">404</h1>
-            <p className="error-text">Oups ! La page que vous demandez n'existe pas.</p>
+            <p className="error-text hiddenMobile">Oups ! La page que vous demandez n'existe pas.</p>
+            <p className="error-text hiddenDesktop">Oups ! La page que <br />vous demandez n'existe pas.</p>
             <Link to="/">Retourner sur la page d'accueil</Link>
         </div>
     );
